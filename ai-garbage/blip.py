958e0c236ffba9dcb62bf2e00b75c74d54b13607
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2026-03-03 11:02:07 -0500

    efs

---
+++ ai-garbage/blip.py
@@ -0,0 +1,27 @@
+import requests
+from PIL import Image
+import torch
+from transformers import Blip2Processor, Blip2ForConditionalGeneration, BitsAndBytesConfig
+
+quant_config = BitsAndBytesConfig(load_in_8bit=False)
+
+processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+
+model = Blip2ForConditionalGeneration.from_pretrained(
+    "Salesforce/blip2-opt-2.7b",
+    device_map="auto",
+    quantization_config=quant_config
+)
+
+img_url = r"https://www.paralympic.org/static-assets/webp/wb_showcase.webp"
+raw_image = Image.open(requests.get(img_url, stream=True).raw).convert("RGB")
+
+question = "Describe the image great detail: The image depicts"
+
+inputs = processor(raw_image, question, return_tensors="pt")
+inputs = {k: v.to(model.device) for k, v in inputs.items()}
+
+with torch.no_grad():
+    output = model.generate(**inputs)
+
+print(processor.decode(output[0], skip_special_tokens=True))
