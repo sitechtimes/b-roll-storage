8541aa22eb3677d8362bdbbf66e409e214fc66a3
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2026-03-18 10:48:09 -0400

    a

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 const mediasRoutes = require('./medias')
+const usersRoutes = require('./users')
+const authRoutes = require('./auth')
 const router = express.Router()
 
 router.use('/medias', mediasRoutes)
+router.use('/users', usersRoutes)
+router.use('/auth', authRoutes)
 
 module.exports = router;
