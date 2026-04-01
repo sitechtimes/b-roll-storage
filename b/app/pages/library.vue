ef417d4a6f1153e72753d866cd6316d338b413cf
Constantine Nagornuk <conplay55@gmail.com>
2026-04-01 10:58:41 -0400

    library modals more work done

--- b/app/pages/library.vue
+++ b/app/pages/library.vue
@@ -6,12 +6,17 @@
     <div v-if="modalView">
       <dialog class="modal" open>
         <div class="modal-box">
-          <h3 class="text-lg font-bold">Hello!</h3>
-          <p class="py-4">Press ESC key or click the button below to close</p>
-          <div class="modal-action">
-            <form method="dialog">
-              <button class="btn" @click="closeLibraryItem()">Close</button>
-            </form>
+          <div>
+            <h3 class="text-lg font-bold">Video Details</h3>
+            <p class="py-4">Title: {{ selectedItem.title }}</p>
+            <img
+              :src="selectedItem.path"
+              :alt="selectedItem.title"
+              class="w-full h-48 object-cover"
+            />
+            <p class="py-4">Type: {{ selectedItem.type }}</p>
+            <p class="py-4">Tags: {{ selectedItem.tags.join(", ") }}</p>
+            <button class="btn" @click="closeLibraryItem()">Close</button>
           </div>
         </div>
       </dialog>
@@ -34,15 +39,6 @@
           <div class="font-bold text-lg mb-2 text-gray-900">
             {{ item.title }}
           </div>
-          <div class="flex flex-wrap justify-center gap-2">
-            <span
-              v-for="tag in item.tags"
-              :key="tag"
-              class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-full"
-            >
-              {{ tag }}
-            </span>
-          </div>
         </div>
       </div>
     </div>
@@ -52,9 +48,11 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 let modalView = ref<boolean>(false);
+let selectedItem = ref<any>(null);
 // leave the type any for now gotta test if it works
 function openLibraryItem(x: any) {
   modalView.value = true;
+  selectedItem.value = x;
   console.log(x);
 }
 function closeLibraryItem() {
