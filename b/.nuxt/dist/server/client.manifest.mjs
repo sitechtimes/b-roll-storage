fa6b49421a6f47ade92ec8c692b915757e5c5308
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2026-04-21 10:45:04 -0400

    i guess b is a good frontend folder name...

--- b/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///C:/Users/constantine.nagornuk/Documents/b-roll-storage/b/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
--- b/.nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Users/constantine.nagornuk/Documents/b-roll-storage/b/node_modules/@nuxt/vite-builder/dist/vite-node-entry.mjs"
