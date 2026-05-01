74fe9cef77a30b1d5febca73625b7e2ae3b4741d
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2026-05-01 10:23:37 -0400

    a

--- backend/routes/medias.ts
+++ backend/routes/medias.ts
@@ -3,11 +3,17 @@ import { requireRole } from "../middleware/validateRole";
 import { UserRole } from "../utils/userRole";
 const mediaController = require("../controllers/mediaController");
 const router = express.Router();
+const upload = require("../middleware/upload");
 
 router.get(`/`, mediaController.index);
 router.get(`/filter`, mediaController.getMedia);
 router.get("/:id", mediaController.getMediaById);
-router.post("/", requireRole(UserRole.Admin), mediaController.createMedia);
+router.post(
+  "/",
+  requireRole(UserRole.Admin),
+  upload.array("photos", 10),
+  mediaController.createMedia,
+);
 router.delete("/:id", requireRole(UserRole.Admin), mediaController.deleteMedia);
 router.delete(`/`, requireRole(UserRole.Admin), mediaController.deleteAllMedia);
 router.patch("/:id", requireRole(UserRole.Admin), mediaController.updateMedia);
